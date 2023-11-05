da994980df3d4d2a48d501ee613edc09102c8669
Rajesh Kanumuru <147418393+rajeshdevopsaws@users.noreply.github.com>
2023-11-05 22:32:47 +0530

    Update main.tf

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -2,7 +2,7 @@
 # creating a resource of type aws_instance
 resource "aws_instance" "app_server" {
     ami             = var.aws_ec2_ami
-    instance_type = var.aws_ec2_ami
+    instance_type = var.aws_ec2_instance_type
     vpc_security_group_ids = [var.vpc_security_group_ids]
     subnet_id = var.public_subnet
     user_data = file("userdata.sh")
